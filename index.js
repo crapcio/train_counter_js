c09e1974ed0f1cc31ac262a8e512157e55d5678a
Your Name <you@example.com>
2024-09-13 23:23:59 +0300

    finished. Added save btn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 let count = 0;
 let countEl = document.getElementById("count-el")
+let welcomeEl = document.getElementById("welcome-el")
+let saveEl = document.getElementById("save-el")
 
 let increment = () => {
     count = count + 1;
-    countEl.innerText = count;
-    console.log(count)
+    countEl.textContent = count;
+}
+
+let save = () => {
+    let countStr = count + " - "
+    saveEl.innerText += countStr
+    console.log(count);
+    count = 0;
+    countEl.textContent = count;
+
 }
